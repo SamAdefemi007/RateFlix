9939928cfb061b2cd25356aa4d7e329313e543ab
SAMUEL OLANIYAN <s.olaniyan.21@abdn.ac.uk>
2022-03-04 11:35:00 +0000

    replicated the movies.html logic for tvshows and kidshows

--- app.py
+++ app.py
@@ -31,3 +31,37 @@ def movies():
     connection.close()
 
     return render_template('movies.html', rows=rows)
+
+
+@app.route('/tvshows')
+def tvshows():
+    connect_db_row()
+    if session.get("logged_in"):
+        cursor.execute("Select * from movie INNER JOIN ratings ON ratings.MOVIE_ID = movie.MOVIE_ID AND movie.TYPE!=\"movie\" ORDER BY ratings.RATING DESC")
+    else:
+        cursor.execute(
+            "Select * from movie INNER JOIN ratings ON ratings.MOVIE_ID = movie.MOVIE_ID AND movie.TYPE!=\"movie\" ORDER BY ratings.RATING DESC LIMIT 10")
+    rows = cursor.fetchall()
+    connection.close()
+
+    return render_template('tvshows.html', rows=rows)
+
+
+@app.route('/kidshow')
+def kidshow():
+    connect_db_row()
+    if session.get("logged_in"):
+        cursor.execute(
+        "Select * from movie INNER JOIN ratings ON ratings.MOVIE_ID = movie.MOVIE_ID WHERE"
+        " movie.TV_RATING!=\"NC-17\" AND movie.TV_RATING!=\"NR\" AND movie.TV_RATING!=\"R\" AND movie.TV_RATING!=\"rating\""
+        "AND movie.TV_RATING!=\"TV-14\" AND movie.TV_RATING!=\"TV-MA\" AND movie.TV_RATING!=\"UR\" ORDER BY ratings.RATING DESC")
+    else:
+        cursor.execute(
+            "Select * from movie INNER JOIN ratings ON ratings.MOVIE_ID = movie.MOVIE_ID WHERE"
+            " movie.TV_RATING!=\"NC-17\" AND movie.TV_RATING!=\"NR\" AND movie.TV_RATING!=\"R\" AND movie.TV_RATING!=\"rating\""
+            "AND movie.TV_RATING!=\"TV-14\" AND movie.TV_RATING!=\"TV-MA\" AND movie.TV_RATING!=\"UR\" ORDER BY ratings.RATING DESC LIMIT 10")
+
+    rows = cursor.fetchall()
+    connection.close()
+
+    return render_template('kidshow.html', rows=rows)
