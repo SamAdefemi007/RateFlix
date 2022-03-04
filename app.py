a611eb31deefe33b46ce65a4970e0e5a67ff4028
SAMUEL OLANIYAN <s.olaniyan.21@abdn.ac.uk>
2022-03-04 10:53:40 +0000

    basic index html page setup

--- app.py
+++ app.py
@@ -3,11 +3,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():  # put application's code here
-    connection = sqlite3.connect(db_name)
+DATABASE = "netflix_dbase.db"
+
+def connect_db_row():
+    global connection, cursor
+    connection = sqlite3.connect(DATABASE)
     connection.row_factory =sqlite3.Row
     cursor=connection.cursor()
+
+@app.route('/')
+def index():  # put application's code here
+    connect_db_row()
     cursor.execute("Select * from movie INNER JOIN ratings ON ratings.MOVIE_ID = movie.MOVIE_ID LIMIT 10")
     rows = cursor.fetchall()
     connection.close()
